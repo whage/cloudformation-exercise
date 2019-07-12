5bad2614519e0083ef39d018b5398a1975ed1302
András Sallai <andras.sallai@evosoft.com>
2019-07-12 16:34:40 +0200

    add more resources

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ resource "aws_vpc" "vpc" {
     cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
 resource "aws_subnet" "subnet_a" {
     vpc_id = "${aws_vpc.vpc.id}"
     cidr_block = "10.0.0.0/24"
@@ -22,8 +26,8 @@ resource "aws_db_subnet_group" "subnet_group" {
     ]
 }
 
-resource "aws_security_group" "sg" {
-    name = "rds-test-sg"
+resource "aws_security_group" "db_sg" {
+    name = "flask-rds-sg"
     vpc_id = "${aws_vpc.vpc.id}"
     
     ingress {
@@ -45,7 +49,7 @@ resource "aws_db_instance" "instance" {
     password = "password1234"
     db_subnet_group_name = "${aws_db_subnet_group.subnet_group.name}"
     skip_final_snapshot = true
-    vpc_security_group_ids = ["${aws_security_group.sg.id}"]
+    vpc_security_group_ids = ["${aws_security_group.db_sg.id}"]
     multi_az = true
 }
 
@@ -60,17 +64,27 @@ resource "aws_autoscaling_group" "asg" {
     ]
 }
 
-/*
 resource "aws_iam_role" "ecs_service_role" {
     name = "flask_ecs_service_role"
     path = "/"
+    assume_role_policy = "${data.aws_iam_policy_document.ecs_service_policy.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_service_role_policy_attachment" {
     role = "${aws_iam_role.ecs_service_role.name}"
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
-*/
+
+data "aws_iam_policy_document" "ecs_service_policy" {
+    statement {
+        actions = ["sts:AssumeRole"]
+
+        principals {
+            type = "Service"
+            identifiers = ["ecs.amazonaws.com"]
+        }
+    }
+}
 
 resource "aws_iam_role" "ecs_instance_role" {
     name = "flask_ecs_instance_role"
@@ -132,6 +146,17 @@ resource "aws_ecs_service" "service" {
     cluster = "${aws_ecs_cluster.cluster.id}"
     task_definition = "${aws_ecs_task_definition.flask_webapp.arn}"
     desired_count = 3
+    iam_role = "${aws_iam_role.ecs_service_role.name}"
+
+    depends_on = [
+        "aws_lb_listener.listener"
+    ]
+
+    load_balancer {
+        target_group_arn = "${aws_lb_target_group.lb_tg.arn}"
+        container_name = "flask_webapp"
+        container_port = 5000
+    }
 }
 
 resource "aws_ecs_task_definition" "flask_webapp" {
@@ -147,10 +172,72 @@ resource "aws_ecs_task_definition" "flask_webapp" {
             "portMappings": [
                 {
                     "containerPort": 5000,
-                    "hostPort": 5000
+                    "hostPort": 80
                 }
             ]
         }
     ]
 EOF
+}
+
+resource "aws_lb" "flask_webapp_load_balancer" {
+    name = "flask-webapp-load-balancer"
+    internal = false
+    load_balancer_type = "application"
+    security_groups = [
+        "${aws_security_group.lb_sg.id}"
+    ]
+    subnets = [
+        "${aws_subnet.subnet_a.id}",
+        "${aws_subnet.subnet_b.id}",
+    ]
+    depends_on = [
+        "aws_internet_gateway.igw"
+    ]
+}
+
+resource "aws_security_group" "lb_sg" {
+    name = "flask-lb-sg"
+    vpc_id = "${aws_vpc.vpc.id}"
+    
+    ingress {
+        from_port = 0
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_lb_target_group" "lb_tg" {
+    name = "flask-webapp-tg"
+    port = "80"
+    protocol = "HTTP"
+    vpc_id = "${aws_vpc.vpc.id}"
+
+    depends_on = [
+        "aws_lb.flask_webapp_load_balancer"
+    ]
+}
+
+resource "aws_lb_listener" "listener" {
+    load_balancer_arn = "${aws_lb.flask_webapp_load_balancer.arn}"
+    port = "80"
+    protocol = "HTTP"
+
+    default_action {
+        target_group_arn = "${aws_lb_target_group.lb_tg.arn}"
+        type = "forward"
+    }
+}
+
+resource "aws_route53_record" "dns_record" {
+  zone_id = "Z1D53ICGMBP5SW"
+  name    = "flask"
+  type    = "A"
+
+  alias {
+    name = "${aws_lb.flask_webapp_load_balancer.dns_name}"
+    zone_id = "Z215JYRZR1TBD5"
+    evaluate_target_health = true
+  }
 }
